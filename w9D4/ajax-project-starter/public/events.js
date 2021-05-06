0767cc8cd35481b3744a549c0e0fbcaa7ed3ef33
Taylor H <taylor@htmlartist.net>
2021-05-06 17:00:02 -0400

    done with phase 2

--- w9D4/ajax-project-starter/public/events.js
+++ w9D4/ajax-project-starter/public/events.js
@@ -1,14 +1,21 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  let catImage = document.querySelector('.cat-pic')
+  let errDiv = document.querySelector('error')
+  let imgButton = document.querySelector('#new-pic')
+  let loadingMessage = document.querySelector("loader");
 
-window.addEventListener("DOMContentLoaded", async(event)=>{
-    let catImage = document.querySelector('.cat-pic')
-        let errDiv = document.querySelector('error')
-        const catImageRes = await fetch('/kitten/image');
-        const resImg = await catImageRes.json();
+  const getCatImage = async() => {
+    const catImageRes = await fetch('/kitten/image');
+    const resImg = await catImageRes.json();
+    catImage.src = resImg.src;
 
-        catImage.src = resImg.src;
+    if (!catImageRes.ok) {
+      //   errDiv.innerHTML = " ERROR ";
+      loadingMessage.innerHTML = "Loading..."
+    }
+  }
+  getCatImage();
+  imgButton.addEventListener("click", getCatImage);
 
-        if(!catImageRes.ok){
-            errDiv.innerHTML = " ERROR ";
-        }
 
 })
