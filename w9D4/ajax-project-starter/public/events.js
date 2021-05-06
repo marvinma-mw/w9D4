c252e099ebe5f6ac9a799e0c9bdb2bf7c741eb62
marvin <marvinma.mw@gmail.com>
2021-05-06 17:45:08 -0400

    Phase 4 Done

--- w9D4/ajax-project-starter/public/events.js
+++ w9D4/ajax-project-starter/public/events.js
@@ -1,21 +1,49 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   let catImage = document.querySelector('.cat-pic')
-  let errDiv = document.querySelector('error')
+  let errDiv = document.querySelector('.error')
   let imgButton = document.querySelector('#new-pic')
-  let loadingMessage = document.querySelector("loader");
+  let loadingMessage = document.querySelector(".loader");
+  let popularityScore = document.querySelector(".score");
+  let upVoteButton = document.querySelector("#upvote");
+  let downVoteButton = document.querySelector("#downvote")
 
+//Section One: GETTING THE IMAGE FROM SERVER
   const getCatImage = async() => {
     const catImageRes = await fetch('/kitten/image');
     const resImg = await catImageRes.json();
     catImage.src = resImg.src;
 
-    if (!catImageRes.ok) {
-      //   errDiv.innerHTML = " ERROR ";
-      loadingMessage.innerHTML = "Loading..."
+    if(!catImageRes.ok){
+    loadingMessage.innerHTML = "Loading..."
+    errDiv.innerHTML="Something went wrong! Please try again!"
     }
   }
+
   getCatImage();
+
   imgButton.addEventListener("click", getCatImage);
 
+//Section Two -1: up vote button
+upVoteButton.addEventListener('click', async(event)=>{
+    upVoteButtonRes = await fetch("/kitten/upvote",{
+        method: "PATCH",
+    })
+    const json = await upVoteButtonRes.json();
+    popularityScore.innerHTML = json.score
+})
+
+//Section Two -2: down vote button
+downVoteButton.addEventListener('click', async(event)=>{
+    downVoteButton = await fetch("/kitten/downvote",{
+        method: "PATCH",
+    })
+    const json = await downVoteButton.json();
+    popularityScore.innerHTML = json.score;
+
+    if(!downVoteButton.ok){
+    errDiv.innerHTML="Something went wrong! Please try again!"
+    }
+
+})
 
 })
